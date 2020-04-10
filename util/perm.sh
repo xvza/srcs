00efdbe14a13e789ca87d9b5a2c0e30b5aac91c8
xvza <>
2020-04-10 19:06:26 +0200

    srcs

--- util/perm.sh
+++ util/perm.sh
@@ -5,9 +5,9 @@ source    "core.sh"  "${2}"  "mode        :"  false
 source    "core.sh"  "${3}"  "file        :"
 source    "foot.sh"
 ###################################################
-[[ -O ${3} ]]    &&                               \
-eval      "${BASH_ALIASES[chmod]} ${@}"
+[[ -O ${3} ]]                                     \
+&& eval   "${BASH_ALIASES[chmod]} ${@}"
 ###################################################
-[[ -O ${3} ]]    ||                               \
-eval      "sudo ${BASH_ALIASES[chmod]} ${@}"
+[[ -O ${3} ]]                                     \
+|| eval   "sudo ${BASH_ALIASES[chmod]} ${@}"
 ###################################################
