c932602961462b45db955591afe916b2fb056e31
xvza <>
2020-04-12 01:09:14 +0200

    srcs

--- user/skel.sh
+++ user/skel.sh
@@ -9,8 +9,8 @@ source    "foot.sh"
 #####################################################################
 source    "del0.sh"  "${HOME}"
 source    "del0.sh"  "${SKEL}"
-source    "dir1.sh"  "${HOME}"                   "${dire[etcs]}skel/"
-source    "dir1.sh"  "${SKEL}"                   "${dire[etcs]}skel/"
+source    "dir0.sh"  "${HOME}"                   "${dire[etcs]}skel/"
+source    "dir0.sh"  "${SKEL}"                   "${dire[etcs]}skel/"
 #####################################################################
 source    "rep0.sh"  "${SKEL}/.config/gtk-3.0/"  "_USER"  "${USER}"
 #####################################################################
