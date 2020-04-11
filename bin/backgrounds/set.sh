9bf4ed6e6ce278d32529ab21117a392c6d9a14e3
xvza <>
2020-04-11 13:01:03 +0200

    srcs

--- bin/backgrounds/set.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-###############################################
-declare   "path=org.gnome.desktop.background"
-declare   "key=picture-uri"
-###############################################
-eval      "feh --bg-fill --no-fehbg ${IMG}"
-eval      "gsettings set ${path} ${key} ${IMG}"
-###############################################
