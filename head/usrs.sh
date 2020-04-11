bdd2ba7530ed145584e6f7c45431eed3636ab22e
xvza <>
2020-04-11 14:35:46 +0200

    srcs

--- head/usrs.sh
+++ head/usrs.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
-#####################################################
+###################################################
 declare   "name=$(basename ${BASH_SOURCE[0]%.*})"
 source    "moun.sh"  "${name}"
-#####################################################
-declare   "dest=${dire[$name]}local"
-eval      "sudo ${BASH_ALIASES[cro]} ${USER} ${dest}"
-#####################################################
-declare   "dest=${dire[$name]}local/bin"
-eval      "sudo ${BASH_ALIASES[crm]} ${dest}"
-#####################################################
-declare   "dire=${dire[$name]}local/share/dconf"
-declare   "file=${dire}/config.ini"
-eval      "dconf reset -f /"
-eval      "dconf load / ${file}"
-#####################################################
+###################################################
+source    "dir0.sh"  "/usr"  "${dire[$name]}/share"
+###################################################
+source    "clea.sh"
+###################################################
--- util/clea.sh
+++ util/clea.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
-###################################################
-function    cl()                                  {
-###################################################
-  eval      "gpaste-client empty"
-  eval      "yay -Scc --noconfirm 2> /dev/null"
-  eval      "dnf clean all 2> /dev/null"
-###################################################
-  declare   "path=${SHARE}/dconf"
-  eval      "dconf reset -f /"
-###################################################
-  declare   "name=config.ini"
-  eval      "dconf load / < ${path}/${name}"
-###################################################
-  declare   "name=desktop.ini"
-  eval      "dconf load / < ${path}/${name}"
-###################################################
-                                                  }
-###################################################
+#############################################
+eval      "gpaste-client empty"
+eval      "yay -Scc --noconfirm 2> /dev/null"
+eval      "dnf clean all 2> /dev/null"
+#############################################
+declare   "path=${SHARE}/dconf"
+eval      "dconf reset -f /"
+#############################################
+declare   "name=config.ini"
+eval      "dconf load / < ${path}/${name}"
+#############################################
+declare   "name=desktop.ini"
+eval      "dconf load / < ${path}/${name}"
+#############################################
