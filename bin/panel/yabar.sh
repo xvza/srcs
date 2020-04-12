c3072e82b86eb920e840d949213d4ac0059318ee
xvza <>
2020-04-13 01:52:19 +0200

    srcs

--- bin/panel/yabar.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#################################################
-declare   "name=$(basename ${BASH_SOURCE[0]%.*})"
-declare   "cfg=${SHRD}/${name}/config.ini"
-#################################################
-eval      "killall -q ${name}"
-eval      "${name} -c ${cfg}"                   &
-#################################################
