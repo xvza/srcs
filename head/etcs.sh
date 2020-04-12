1df617869e22f3dedbb0f7c75c6f491ee0a4ae33
xvza <>
2020-04-12 02:03:56 +0200

    srcs

--- head/etcs.sh
+++ head/etcs.sh
@@ -1,27 +1,24 @@
 #!/usr/bin/env bash
-#################################################
+######################################################
 declare   "name=$(basename ${BASH_SOURCE[0]%.*})"
 source    "moun.sh"  "${name}"
-#################################################
+######################################################
 declare   "dest=${HOME}/"
 eval      "sudo del0.sh ${dest}"
-#################################################
-declare   "dest=/etc/skel/"
+######################################################
+declare   "dest=/etc/skel"
 eval      "sudo del0.sh ${dest}"
-#################################################
-declare   "dest=/etc/"
+######################################################
+declare   "dest=/etc"
 declare   "src=${dire[$name]}"
 eval      "sudo dir0.sh ${dest} ${src}"
-#################################################
-declare   "dest=${HOME}/.config/"
-declare   "src=/etc/skel/.config"
-eval      "sudo dir0.sh ${dest} ${src}"
-#################################################
-declare   "dest=${HOME}/.local/share/"
-declare   "src=/etc/skel/.local/share/"
-eval      "sudo dir0.sh ${dest} ${src}"
-#################################################
-declare   "dest=${dest}bashrc"
+######################################################
+declare   "dest=/etc/bashrc"
 source    "${dest}"
 echo      "source: ${dest}"
-#################################################
+######################################################
+declare   "dest=${HOME}"
+declare   "src=/etc/skel"
+declare   "opt=-mindepth 1 -maxdepth 1 -name .*"
+eval      "sudo dir0.sh ${dest} $(find ${src} ${opt})"
+######################################################
