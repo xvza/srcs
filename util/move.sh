9236b373cf62c04f5d078637678b871171c994c8
xvza <>
2020-04-13 17:05:47 +0200

    srcs

--- util/move.sh
+++ util/move.sh
@@ -6,8 +6,11 @@ declare   "-i"  "pos=${WIDTH}/2"
 eval      "xdotool windowsize ${id} 50% 92%"
 #############################################
 [[ ${1?} -eq 0 ]]                           \
-&& eval   "xdotool windowmove ${id} ${1} y"
+&& eval   "xdotool windowsize ${id} 100% 92%"
 #############################################
 [[ ${1?} -eq 1 ]]                           \
+&& eval   "xdotool windowmove ${id} 0 y"
+#############################################
+[[ ${1?} -eq 2 ]]                           \
 && eval   "xdotool windowmove ${id} ${pos} y"
 #############################################
