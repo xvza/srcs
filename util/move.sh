6f7e8dad4430a7990ecd9db12cc4d782f36a86dc
xvza <>
2020-04-13 18:07:54 +0200

    srcs

--- util/move.sh
+++ util/move.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 #############################################
 declare   "id=$(xdotool getactivewindow)"
-declare   "-i"  "pos=${WIDTH}/2"
+declare   "-i"  "pos=${1?}/2"
 #############################################
-eval      "xdotool windowsize ${id} 50% 92%"
+eval      "xdotool windowmove ${id} ${pos} y"
 #############################################
-[[ ${1?} -eq 0 ]]                           \
-&& eval   "xdotool windowsize ${id} 100% 92%"
-#############################################
-[[ ${1?} -eq 1 ]]                           \
-&& eval   "xdotool windowmove ${id} 0 y"
-#############################################
-[[ ${1?} -eq 2 ]]                           \
-&& eval   "xdotool windowmove ${id} ${pos} y"
+source    "size.sh"  "50"  "92"
 #############################################
