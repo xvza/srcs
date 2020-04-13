5665060b04f942de211016465192d9c9b8b9fb52
xvza <>
2020-04-13 03:08:11 +0200

    srcs

--- util/xrdb.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#######################################
-declare   "s0=xresource directory"
-source    "arrf.sh"  "${1?$s0}"
-declare   "name=$(basename ${1})"
-#######################################
-eval      "$(${name} <(cat ${file[@]}))"
-#######################################
