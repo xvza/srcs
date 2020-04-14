fa85f577dd449377e61d8f0de9e85aca1258f29d
xvza <>
2020-04-15 00:04:47 +0200

    srcs

--- axdg/background.sh
+++ axdg/background.sh
@@ -5,8 +5,8 @@ declare   "url=https://source.unsplash.com/${arg}"
 declare   "path=org.gnome.desktop.background"
 declare   "img=/etc/skel/Picture/_.jpg"
 ####################################################
-source    "net2.sh"  "${img}"  "${url}"  "${img}"
-####################################################
 eval      "feh --bg-fill --no-fehbg ${img}"
 eval      "gsettings set ${path} picture-uri ${img}"
+####################################################
+source    "net2.sh"  "${img}"  "${url}"  "${img}"
 ####################################################
