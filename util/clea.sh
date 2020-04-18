452e2692d682b2df3ede9277ac944b338b9321d2
xvza <>
2020-04-18 16:44:13 +0200

    srcs

--- util/clea.sh
+++ util/clea.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
-#############################################
+###############################################
 eval      "gpaste-client empty"
 eval      "yay -Scc --noconfirm 2> /dev/null"
 eval      "dnf clean all 2> /dev/null"
-#############################################
+###############################################
 declare   "path=${SHRD}/dconf"
 eval      "dconf reset -f /"
-#############################################
-declare   "name=config.ini"
-eval      "dconf load / < ${path}/${name}"
-#############################################
-declare   "name=desktop.ini"
-eval      "dconf load / < ${path}/${name}"
-#############################################
+###############################################
+declare   "list=find ${path} -type f"
+###############################################
+eval      "dconf load / < $(${list} | head -1)"
+eval      "dconf load / < $(${list} | tail -1)"
+###############################################
