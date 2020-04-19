a3ceeac054b542e96b50086f40780a0fb99d6342
xvza <>
2020-04-19 13:41:04 +0200

    srcs

--- bash/alias.sh
+++ bash/alias.sh
@@ -1,44 +1,38 @@
 #!/usr/bin/env bash
-################################################################
+################################################
 alias   "le=cd /etc/"
+alias   "lu=cd /usr/"
+alias   "lv=cd /var/"
+################################################
 alias   "lx=le;cd ./xdg/"
 alias   "lk=le;cd ./skel/"
+alias   "lh=lv;cd ./http/"
+################################################
 alias   "lea=lx;cd ./autostart/"
 alias   "leo=lx;cd ./openbox/"
-################################################################
-alias   "lu=cd /usr/"
 alias   "lua=lu;cd ./share/"
 alias   "lui=lua;cd ./icons/"
 alias   "lut=lua;cd ./themes/"
-################################################################
-alias   "lv=cd /var/"
-alias   "lvh=lv;cd ./http/"
-################################################################
-#alias   "rd=rt devs"
-#alias   "re=rt etcs"
-#alias   "rh=rt trsh"
-#alias   "rl=rt list"
-#alias   "ra=rt shrd"
-#alias   "rs=rt srcs"
-#alias   "ru=rt usrs"
-################################################################
+################################################
+alias   "rd=rt devs"
+alias   "re=rt etcs"
+alias   "rh=rt trsh"
+alias   "rl=rt list"
+alias   "ra=rt shrd"
+alias   "rs=rt srcs"
+alias   "ru=rt usrs"
+################################################
 alias   "rk=re;cd ./skel"
 alias   "rx=re;cd ./xdg/"
+################################################
 alias   "rea=rx;cd ./autostart/"
 alias   "reo=rx;cd ./openbox/"
-################################################################
-alias   "rb=rs;cd ./bin/bash/"
-################################################################
-alias   "cp=cpo -u"
-alias   "cpo=cp -rv"
-################################################################
+alias   "rsa=rs;cd ./axdg/"
+alias   "rsb=rs;cd ./bash/"
+################################################
 alias   "chmod=chmod -v"
 alias   "chown=chown -v"
-################################################################
-alias   "en=ta :en"
-alias   "fr=ta :fr"
-alias   "ta=trans -p"
-################################################################
+alias   "cp=cp -urv"
 alias   "du=du -hsc"
 alias   "grep=grep --color=auto -i"
 alias   "ls=exa -la -s type"
@@ -46,13 +40,13 @@ alias   "mkdir=mkdir -vp"
 alias   "mv=mv -v"
 alias   "rename=rename -v"
 alias   "rm=rm -rv"
-################################################################
+################################################
 alias   "newsbeuter=rl;newsbeuter -u ./news/add"
 alias   "cava=ra;cava -p ./cava/config.ini"
-################################################################
+################################################
 alias   "rofi=rofi.sh"
 alias   "polybar=polybar.sh"
-################################################################
+################################################
 alias   "gr=grep"
 alias   "ft=grep -nr"
 alias   "ge=git clean -f -x"
@@ -60,4 +54,8 @@ alias   "lt=ll -T"
 alias   "ne=neofetch"
 alias   "wg=wget"
 alias   "wh=which"
-################################################################
+################################################
+alias   "en=ta :en"
+alias   "fr=ta :fr"
+alias   "ta=trans -p"
+################################################
