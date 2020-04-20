86f6683566f9027238f94cc6e2f599eaefad1f45
xvza <>
2020-04-20 22:45:26 +0200

    srcs

--- head.sh
+++ head.sh
@@ -4,6 +4,6 @@ declare   "dire=$(dirname ${BASH_SOURCE[0]})"
 source    "${dire}/dirs.sh"
 declare   "name=$(basename ${BASH_SOURCE[0]%.*})"
 #################################################
-source    "${dire}/arrf.sh"  "${srcs[$name]}"
+source    "arrf.sh"  "${srcs[$name]}"
 source    <(cat ${file[@]})
 #################################################
