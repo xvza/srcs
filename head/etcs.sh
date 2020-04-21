b856b13b9ffdc8a84606e2a5875049e962a4560b
xvza <>
2020-04-21 12:27:33 +0200

    srcs

--- head/etcs.sh
+++ head/etcs.sh
@@ -5,11 +5,13 @@ declare   "opts=-mindepth 1 -maxdepth 1"
 declare   "hide=-name .*"
 declare   "dele=$(command -v dele.sh)"
 declare   "copy=$(command -v dir0.sh)"
+declare   "env=profile.d/environment.sh"
 #################################################
 source    "moun.sh"  "${name}"
+source    "${dire[$name]}/${env}"
 #################################################
 eval      "sudo ${dele} ${HOME}"
-eval      "sudo ${dele} /etc/skel"
+eval      "sudo ${dele} ${SKEL}"
 #################################################
 declare   "dest=${dire[$name]} ${opts} ! ${hide}"
 eval      "sudo ${copy} /etc $(find ${dest})"
