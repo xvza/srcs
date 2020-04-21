53cef87a22beb2a70373f5344215704d9fd012d3
xvza <>
2020-04-21 11:54:58 +0200

    srcs

--- head/etcs.sh
+++ head/etcs.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 #################################################
 declare   "name=$(basename ${BASH_SOURCE[0]%.*})"
-declare   "opts=-mindepth 1 -maxdepth 1 -name .*"
-declare   "cmd=${SKEL} ${opts}"
+declare   "opts=-mindepth 1 -maxdepth 1"
+declare   "hide=-name .*"
 declare   "dele=$(command -v dele.sh)"
-declare   "dir0=$(command -v dir0.sh)"
+declare   "copy=$(command -v dir0.sh)"
 #################################################
 source    "moun.sh"  "${name}"
 #################################################
 eval      "sudo ${dele} ${HOME}"
 eval      "sudo ${dele} /etc/skel"
-eval      "sudo ${dir0} /etc ${dire[$name]}/*"
-eval      "sudo ${dir0} ${HOME} $(find ${cmd})"
+#################################################
+declare   "dest=${dire[$name]} ${opts} ! ${hide}"
+eval      "sudo ${copy} /etc $(find ${dest})"
+#################################################
+declare   "dest=${SKEL} ${opts} ${hide}"
+eval      "sudo ${copy} ${HOME} $(find ${dest})"
 #################################################
 declare   "dest=/etc/bashrc"
 source    "${dest}"
