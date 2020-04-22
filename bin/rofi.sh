c94826f4f0d6a92e4f4e53bd9e4dc9a8d58e22bc
xvza <>
2020-04-22 22:20:24 +0200

    srcs

--- bin/rofi.sh
+++ bin/rofi.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-####################################################################
+#########################################################
 source    "colo.sh"
-####################################################################
+#########################################################
 declare   "name=$(basename ${BASH_SOURCE[0]%.*})"
-declare   "window=${background},${border},${primary[background]}"
-declare   "color=${background},${primary[foreground]},${background}"
-declare   "color=${color},${primary[background]},${background}"
-####################################################################
-${name}   "-color-window"  "${window}"                             \
-          "-color-normal"  "${color}"                              \
-          "-color-active"  "${color}"                              \
+declare   "b=${hexa[background]}"
+declare   "p=${hexa[primary]}"
+declare   "f=${hexa[foreground]}"
+#########################################################
+${name}   "-color-window"  "${b},${hexa[border]},${p}"  \
+          "-color-normal"  "${b},${f},${b},${p},${b}"   \
+          "-color-active"  "${b},${f},${b},${p},${b}"   \
           "-show"
-####################################################################
+#########################################################
--- util/colo.sh
+++ util/colo.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 #######################################
-declare   "bold=$(tput bold)"
-declare   "nc=$(tput sgr0)"
-#######################################
 declare   "-A"  "shell"
 #######################################
+declare   "shell[bold]=$(tput bold)"
+declare   "shell[init]=$(tput init)"
 declare   "shell[0]=$(tput setaf 2)"
 declare   "shell[1]=$(tput setaf 1)"
 declare   "shell[2]=$(tput setaf 5)"
 declare   "shell[3]=$(tput setaf 4)"
 declare   "shell[1000]=$(tput setaf 6)"
 #######################################
-declare   "-A"  "primary"
+declare   "-A"  "hexa"
 #######################################
-declare   "border=#2e3440"
-declare   "background=#3b4252"
-declare   "primary[foreground]=#d8dee9"
-declare   "primary[background]=#8fbcbb"
+declare   "hexa[border]=#2e3440"
+declare   "hexa[background]=#3b4252"
+declare   "hexa[foreground]=#d8dee9"
+declare   "hexa[primary]=#8fbcbb"
 #######################################
