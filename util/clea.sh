89b73e52a03a2e01b91f9c984ef563e78aa22f88
xvza <>
2020-04-22 14:43:22 +0200

    srcs

--- util/clea.sh
+++ util/clea.sh
@@ -4,11 +4,16 @@ eval      "gpaste-client empty"
 eval      "yay -Scc --noconfirm 2> /dev/null"
 eval      "dnf clean all 2> /dev/null"
 ###############################################
-declare   "path=${SHRD}/dconf"
+declare   "list=find ${SHRD}/dconf -type f"
 eval      "dconf reset -f /"
-###############################################
-declare   "list=find ${path} -type f"
-###############################################
 eval      "dconf load / < $(${list} | head -1)"
 eval      "dconf load / < $(${list} | tail -1)"
+###############################################
+declare   "dire=${HOME}/.local/share/epiphany"
+declare   "opts=-mindepth 1 ! -name *.gvdb"
+eval      "find ${dire} ${opts} -delete"
+###############################################
+declare   "dire=${HOME}/.local/share/"
+declare   "opts=-name *.xbel"
+eval      "find ${dire} ${opts} -delete"
 ###############################################
