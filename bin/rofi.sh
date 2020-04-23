ee6deffebe12d334764c07005a1d0c34218336f7
xvza <>
2020-04-23 16:51:37 +0200

    srcs

--- bin/rofi.sh
+++ bin/rofi.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
-#########################################################
+#############################################################
 source    "colo.sh"
-#########################################################
+#############################################################
 declare   "name=$(basename ${BASH_SOURCE[0]%.*})"
-declare   "b=${hexa[background]}"
-declare   "p=${hexa[primary]}"
-declare   "f=${hexa[foreground]}"
-#########################################################
-${name}   "-color-window"  "${b},${hexa[border]},${p}"  \
-          "-color-normal"  "${b},${f},${b},${p},${b}"   \
-          "-color-active"  "${b},${f},${b},${p},${b}"   \
+declare   "hb=${hexa[background]}"
+declare   "hp=${hexa[primary]}"
+declare   "hf=${hexa[foreground]}"
+declare   "hr=${hexa[border]}"
+#############################################################
+command   "${name}"                                         \
+          "-color-window"  "${hb},${hr},${hp}"              \
+          "-color-normal"  "${hb},${hf},${hb},${hp},${hb}"  \
+          "-color-active"  "${hb},${hf},${hb},${hp},${hb}"  \
           "-show"
-#########################################################
+#############################################################
--- util/colo.sh
+++ util/colo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#######################################
+###########################################
 declare   "-A"  "shell"
-#######################################
+###########################################
 declare   "shell[bold]=$(tput bold)"
 declare   "shell[init]=$(tput init)"
 declare   "shell[0]=$(tput setaf 2)"
@@ -9,11 +9,18 @@ declare   "shell[1]=$(tput setaf 1)"
 declare   "shell[2]=$(tput setaf 5)"
 declare   "shell[3]=$(tput setaf 4)"
 declare   "shell[1000]=$(tput setaf 6)"
-#######################################
+###########################################
 declare   "-A"  "hexa"
-#######################################
+###########################################
 declare   "hexa[border]=#2e3440"
 declare   "hexa[background]=#3b4252"
 declare   "hexa[foreground]=#d8dee9"
 declare   "hexa[primary]=#8fbcbb"
-#######################################
+###########################################
+declare   "-a"  "background"
+###########################################
+for i in {0..15}
+do
+  declare "background[$i]=$(tput setab $i)"
+done
+###########################################
