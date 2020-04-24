097d4d9424b61999950e0e358f92d4cee20accf5
xvza <>
2020-04-25 00:54:34 +0200

    srcs

--- head.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#################################################
-declare   "dire=$(dirname ${BASH_SOURCE[0]})"
-source    "${dire}/head/dirs.sh"
-declare   "name=$(basename ${BASH_SOURCE[0]%.*})"
-#################################################
-source    "arrf.sh"  "${srcs[$name]}"
-source    <(cat ${file[@]})
-#################################################
