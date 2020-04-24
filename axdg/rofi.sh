34fe31972eb4de52a5b9b65146c4501903960c90
xvza <>
2020-04-24 20:49:48 +0200

    srcs

--- axdg/rofi.sh
+++ axdg/rofi.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
-##########################################################
+#############################################################
 declare   "name=$(basename ${BASH_SOURCE[0]%.*})"
-declare   "file=$(find ${SHRD}/${name} -type f)"
 declare   "color=${SHRD}/xrdb/color/${name}.ini"
-##########################################################
+#############################################################
 source    "colo.sh"
-##########################################################
+#############################################################
 declare   "hb=${hexa[background]}"
 declare   "hp=${hexa[primary]}"
 declare   "hf=${hexa[foreground]}"
 declare   "hr=${hexa[border]}"
-##########################################################
-echo      ""                                                   >   ${color}
-echo      "rofi.color-window  :${hb},${hr},${hp}"              >>  ${color}
-echo      "rofi.color-normal  :${hb},${hf},${hb},${hp},${hb}"  >>  ${color}
-echo      "rofi.color-active  :${hb},${hf},${hb},${hp},${hb}"  >>  ${color}
-##########################################################
+#############################################################
+declare   "color_window=${hb},${hr},${hp}"
+declare   "color_normal=${hb},${hf},${hb},${hp},${hb}"
+declare   "color_active=${hb},${hf},${hb},${hp},${hb}"
+#############################################################
+printf    ""                                     >   ${color}
+echo      "rofi.color-window  :${color_window}"  >>  ${color}
+echo      "rofi.color-normal  :${color_normal}"  >>  ${color}
+echo      "rofi.color-active  :${color_active}"  >>  ${color}
+#############################################################
