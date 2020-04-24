dcdc60d097beca5c12c318aed377d34f7591490c
xvza <>
2020-04-24 20:33:23 +0200

    srcs

--- axdg/rofi.sh
+++ axdg/rofi.sh
@@ -2,12 +2,17 @@
 ##########################################################
 declare   "name=$(basename ${BASH_SOURCE[0]%.*})"
 declare   "file=$(find ${SHRD}/${name} -type f)"
-declare   "color=${SHRD}/${name}/color.ini"
+declare   "color=${SHRD}/xrdb/color/${name}.ini"
 ##########################################################
 source    "colo.sh"
 ##########################################################
 declare   "hb=${hexa[background]}"
+declare   "hp=${hexa[primary]}"
+declare   "hf=${hexa[foreground]}"
+declare   "hr=${hexa[border]}"
 ##########################################################
-echo      ""                                  >   ${color}
-echo      "rofi.color-window  :${hexa[foreground]}"  >>  ${color}
+echo      ""                                                   >   ${color}
+echo      "rofi.color-window  :${hb},${hr},${hp}"              >>  ${color}
+echo      "rofi.color-normal  :${hb},${hf},${hb},${hp},${hb}"  >>  ${color}
+echo      "rofi.color-active  :${hb},${hf},${hb},${hp},${hb}"  >>  ${color}
 ##########################################################
