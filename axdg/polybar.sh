1ab6de4e867c03392a6f50961c43dc5dc06076d5
xvza <>
2020-04-25 13:44:43 +0200

    srcs

--- axdg/polybar.sh
+++ axdg/polybar.sh
@@ -5,7 +5,7 @@ declare   "file=$(find ${SHRD}/${name} -type f)"
 declare   "color=${SHRD}/${name}/color.ini"
 ######################################################
 eval      "source colo.sh"
-eval      "source ${SHRD}/${name}/color.sh > ${color}"
+eval      "source poly.sh > ${color}"
 ######################################################
 eval      "killall -q ${name}"
 eval      "$(${name} -c  <(cat ${file})  bottom)"    &
---
+++ util/poly.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#######################################
+echo "[color]"
+echo "foreground  =${hexa[foreground]}"
+echo "background  =${hexa[border]}"
+echo "border      =${hexa[border]}"
+echo "success     =${hexa[primary]}"
+echo "warning     =${hexa[warning]}"
+echo "alert       =${hexa[alert]}"
+#######################################
