37b9bcef555c2581441520cf84e5d996cc83c556
xvza <>
2020-04-26 17:20:17 +0200

    srcs

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -8,6 +8,6 @@ unset     "PS1"
 declare   "PS1+=$(eval ${line})\n"
 declare   "PS1+=\[${shell[bold]}\]"
 declare   "PS1+=\[${shell[$id]}\]"
-declare   "PS1+=[\u:\w:\#]"
+declare   "PS1+=⸤\u:\w:\#⸣"
 declare   "PS1+=\[${shell[init]}\] "
 ####################################
