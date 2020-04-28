a0415774b673a3737027b58d6db9189f8331cc2c
xvza <>
2020-04-28 12:38:10 +0200

    srcs

--- util/colo.sh
+++ util/colo.sh
@@ -3,12 +3,12 @@
 declare   "-A"  "shell"
 ###########################################
 declare   "shell[bold]=$(tput bold)"
-declare   "shell[init]=$(tput init)"
+declare   "shell[init]=$(tput sgr0)"
 declare   "shell[0]=$(tput setaf 2)"
 declare   "shell[1]=$(tput setaf 1)"
 declare   "shell[2]=$(tput setaf 5)"
 declare   "shell[3]=$(tput setaf 4)"
-declare   "shell[primary]=$(tput setaf 10)"
+declare   "shell[primary]=$(tput setaf 6)"
 ###########################################
 declare   "-A"  "hexa"
 ###########################################
