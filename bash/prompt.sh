eb80e653fd533611b529dd87b86d8adc8a67b34b
xvza <>
2020-04-28 14:02:31 +0200

    srcs

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -6,7 +6,7 @@ declare   "id=$(id -u ${USER})"
 ######################################
 unset     "PS1"
 ######################################
-declare   "PS1+=$(eval ${line})\n"
+declare   "PS1+=$(eval ${LINE})\n"
 declare   "PS1+=\[${shell[bold]}\]"
 declare   "PS1+=\[${shell[primary]}\]"
 declare   "PS1+=⸤${id}:\u:\w:\#⸣"
