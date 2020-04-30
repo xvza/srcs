250e7e6197880089f558072281660851007797a9
xvza <>
2020-04-30 23:08:07 +0200

    srcs

--- dirs.sh
+++ dirs.sh
@@ -2,17 +2,17 @@
 #############################################
 declare   "srcs=$(dirname ${BASH_SOURCE[0]})"
 declare   "http=${srcs}/.."
-declare   "shel=${srcs}/bash.d"
+declare   "shel=${srcs}/shel.d"
 #############################################
 source    "${srcs}/util.d/arrd.sh"  "${srcs}"
 declare   "path=$(echo ${dire[@]})"
 export    "PATH=$(getconf PATH):${path// /:}"
 #############################################
-source    "arrf.sh"  "${shel}"
+source    "arrf.sh"                 "${shel}"
 source    "dircolors.sh"
 source    "terminal.sh"
 #############################################
-eval      "clear"
+command   "clear"
 #############################################
 source    "environment.sh"
 source    "palette.sh"
