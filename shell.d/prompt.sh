b56d477242b222ee6131a42d679a4bc545a76124
xvza <>
2020-05-01 00:21:53 +0200

    srcs

--- shell.d/prompt.sh
+++ shell.d/prompt.sh
@@ -6,6 +6,6 @@ declare   "id=$(id -u ${USER})"
 declare   "PS1=$(eval ${LINE})\n"
 declare   "PS1+=${shell[bold]}"
 declare   "PS1+=${shell[primary]}"
-declare   "PS1+=⸤\u:${id}:\w:\#⸣"
+declare   "PS1+=\[⸤\u:${id}:\w:\#⸣\]"
 declare   "PS1+=${shell[init]} "
 ######################################
--- source.d/shel.sh
+++ source.d/shel.sh
@@ -2,11 +2,11 @@
 ##############################################################
 declare   "-A"  "shell"
 ##############################################################
-declare   "shell[bold]=\[$(setterm -bold on)\]"
-declare   "shell[init]=\[$(setterm -default)\]"
-declare   "shell[0]=\[$(setterm -foreground green)\]"
-declare   "shell[1]=\[$(setterm -foreground red)\]"
-declare   "shell[2]=\[$(setterm -foreground magenta)\]"
-declare   "shell[3]=\[$(setterm -foreground blue)\]"
-declare   "shell[primary]=\[$(setterm -foreground ${COLOR})\]"
+declare   "shell[bold]=$(setterm -bold on)"
+declare   "shell[init]=$(setterm -default)"
+declare   "shell[0]=$(setterm -foreground green)"
+declare   "shell[1]=$(setterm -foreground red)"
+declare   "shell[2]=$(setterm -foreground magenta)"
+declare   "shell[3]=$(setterm -foreground blue)"
+declare   "shell[primary]=$(setterm -foreground ${COLOR})"
 ##############################################################
