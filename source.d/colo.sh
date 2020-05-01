23d185ffc3e2d0912c2f84d05f5c556cac75734f
xvza <>
2020-05-01 17:34:53 +0200

    srcs

--- source.d/colo.sh
+++ source.d/colo.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-#############################################
+###########################################################
 declare   "-A"  "color"
 declare   "-A"  "share"
 declare   "-a"  "order"
-#############################################
+###########################################################
 declare   "color[black]=2e3440"
 declare   "color[red]=bf616a"
 declare   "color[green]=a3be8c"
@@ -13,13 +13,13 @@ declare   "color[magenta]=b48ead"
 declare   "color[cyan]=8fbcbb"
 declare   "color[white]=d8dee9"
 declare   "share[background]=3b4252"
-#############################################
+###########################################################
 declare   "share[border]=${color[black]}"
 declare   "share[foreground]=${color[white]}"
 declare   "share[primary]=${color[$COLOR]}"
 declare   "share[warning]=${color[yellow]}"
 declare   "share[alert]=${color[red]}"
-#############################################
+###########################################################
 declare   "order+=('${color[black]}')"
 declare   "order+=('${color[red]}')"
 declare   "order+=('${color[green]}')"
@@ -28,4 +28,4 @@ declare   "order+=('${color[blue]}')"
 declare   "order+=('${color[magenta]}')"
 declare   "order+=('${color[cyan]}')"
 declare   "order+=('${color[white]}')"
-#############################################
+###########################################################
