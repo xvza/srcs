261426b250c9d7e78ce114cf4d62b2a522d47586
xvza <>
2020-05-01 11:53:45 +0200

    srcs

--- shell.d/function/http.sh
+++ shell.d/function/http.sh
@@ -1,28 +1,27 @@
 #!/usr/bin/env bash
-########################################################
-function    gl()                                       {
-########################################################
+###########################################################
+function  gl()                                            {
+###########################################################
   declare   "dest=${2-$TMPDIR}/${1}"
   declare   "url=https://github.com/${1}.git"
-########################################################
+###########################################################
   source    "head.sh"
   source    "core.sh"  "${dest}"  "destination :"  false
   source    "core.sh"  "${url}"   "source      :"  false
   source    "foot.sh"
-########################################################
   source    "net1.sh"  "${dest}"  "${url}"
-########################################################
-                                                       }
-########################################################
-function    gh()                                       {
-########################################################
-  git checkout --orphan latest_branch
-  git add -A
-  git commit -am "initial commit"
-  git branch -D master
-  git branch -m master
-  git push -f origin master
-  git push --set-upstream origin master
-########################################################
-                                                       }
-########################################################
+###########################################################
+                                                          }
+###########################################################
+function  gh()                                            {
+###########################################################
+  eval      "git checkout --orphan latest_branch"
+  eval      "git add -A"
+  eval      "git commit -am initial commit"
+  eval      "git branch -D master"
+  eval      "git branch -m master"
+  eval      "git push -f origin master"
+  eval      "git push --set-upstream origin master"
+###########################################################
+                                                          }
+###########################################################
