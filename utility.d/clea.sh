e1bb849732ff27647a2eca7007ec4a5c50e69ec9
xvza <>
2020-05-04 15:53:35 +0000

    srcs

--- utility.d/clea.sh
+++ utility.d/clea.sh
@@ -18,3 +18,5 @@ eval      "delete ${HOME}/.local/share/epiphany ! -name *.gvdb"
 eval      "delete ${HOME}/.local/share -name *.xbel"
 eval      "delete ${HOME}/.cache"
 ###########################################################
+source    "background.sh"
+###########################################################
