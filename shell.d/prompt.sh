0ee7ec754d2f41c859bb621fa624f3bdc29d2e08
xvza <>
2020-05-05 18:10:25 +0200

    srcs

--- shell.d/prompt.sh
+++ shell.d/prompt.sh
@@ -4,7 +4,7 @@ source    "shel.sh"
 ######################################
 declare   "PS1=$(eval ${LINE})\n"
 declare   "PS1+=${shell[bold]}"
-declare   "PS1+=${shell[primary]}"
-declare   "PS1+=\[⸤\u:\w:\#⸣\]"
+declare   "PS1+=\[${shell[primary]}\]"
+declare   "PS1+=⸤\u:\w:\#⸣"
 declare   "PS1+=${shell[init]} "
 ######################################
