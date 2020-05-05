d10b66139f332693d8f0235e105ec7f4d1501c01
xvza <>
2020-05-05 18:08:58 +0200

    srcs

--- shell.d/prompt.sh
+++ shell.d/prompt.sh
@@ -2,10 +2,9 @@
 ######################################
 source    "shel.sh"
 ######################################
-declare   "id=$(id -u ${USER})"
 declare   "PS1=$(eval ${LINE})\n"
 declare   "PS1+=${shell[bold]}"
 declare   "PS1+=${shell[primary]}"
-declare   "PS1+=\[⸤\u:${id}:\w:\#⸣\]"
+declare   "PS1+=\[⸤\u:\w:\#⸣\]"
 declare   "PS1+=${shell[init]} "
 ######################################
