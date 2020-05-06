cae64716eed449dcbcd9302305319eb377d517a3
xvza <>
2020-05-06 14:39:59 +0200

    srcs

--- program.d/dnf.sh
+++ program.d/dnf.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-########################################
+###########################################################
 declare   "del=autoremove -y"
 declare   "add=install --skip-broken -y"
 declare   "upd=upgrade -y"
-########################################
+###########################################################
 declare   "li=list installed"
 declare   "la=list available"
 declare   "le=list extras"
-########################################
+###########################################################
 declare   "qs=search"
 declare   "qi=info"
 declare   "qp=provides"
 declare   "qh=history"
-########################################
+###########################################################
--- source.d/shel.sh
+++ source.d/shel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-##########################################################
+###########################################################
 declare   "-A"  "shell"
-##########################################################
+###########################################################
 declare   "shell[bold]=$(setterm -bold on)"
 declare   "shell[init]=$(setterm -default)"
 declare   "shell[0]=$(setterm -foreground green)"
@@ -9,4 +9,4 @@ declare   "shell[1]=$(setterm -foreground red)"
 declare   "shell[2]=$(setterm -foreground magenta)"
 declare   "shell[3]=$(setterm -foreground blue)"
 declare   "shell[primary]=$(setterm -foreground ${COLOR})"
-##########################################################
+###########################################################
