3931e6fc6c9b98f831f9926def290d8584a03f82
xvza <>
2020-05-06 19:45:50 +0200

    srcs

--- utility.d/xdotool.sh
+++ utility.d/xdotool.sh
@@ -2,9 +2,6 @@
 ###########################################################
 declare   "name=$(basename ${BASH_SOURCE[0]%.*})"
 declare   "id=$(${name} getwindowfocus)"
-
-
-echo      "${name} window${1} ${id} ${@:2}"
 eval      "${name} window${1} ${id} ${@:2}"
 ###########################################################
 [[ ${1} == "move" ]] && eval "xdotool.sh size 50% 100%"
