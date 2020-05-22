103fb5fddc9a9466230b12f0d726ac45eba3cdf7
xvza <>
2020-05-22 18:42:30 +0200

    srcs

--- autostart.d/environment.sh
+++ autostart.d/environment.sh
@@ -8,7 +8,6 @@ export      "WIDTH=1920"
 export      "HEIGHT=1080"
 export      "COLOR=cyan"
 export      "SKEL=/etc/skel"
-export      "SHRD=${mnt}/xvza/shrd"
 ###########################################################
 export      "LINE=seq -s─ $(($(tput cols) -1))|tr -d [:digit:]"
 ###########################################################
