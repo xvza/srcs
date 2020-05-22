f2165651ed9b8bd3fefb6609fbc64565714c57c5
xvza <>
2020-05-22 23:55:54 +0200

    srcs

--- autostart.d/environment.sh
+++ autostart.d/environment.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 ###########################################################
+set +u
+###########################################################
 export      "PATH=${PATH}"
 export      "TMPDIR=/tmp"
 export      "EDITOR=nano"
@@ -8,9 +10,12 @@ export      "WIDTH=1920"
 export      "HEIGHT=1080"
 export      "COLOR=cyan"
 export      "SKEL=/etc/skel"
+export	    "SHRD=${mnt}/shrd"
 ###########################################################
 export      "LINE=seq -s─ $(($(tput cols) -1))|tr -d [:digit:]"
 ###########################################################
 source      "/usr/share/bash-completion/bash_completion"
 source      "/etc/bash_completion"
+###########################################################
+set -u
 ###########################################################
