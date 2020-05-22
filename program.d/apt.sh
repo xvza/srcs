082862facd7a71e916b93eb314c40154246df4ea
xvza <>
2020-05-22 14:35:49 +0200

    srcs

--- program.d/apt.sh
+++ program.d/apt.sh
@@ -4,6 +4,9 @@ declare   "del= autoremove --purge -y"
 declare   "add= install -y"
 declare   "upd= upgrade -y"
 ###########################################################
+declare   "li= list --installed"
+declare   "la= list"
+###########################################################
 declare   "qs= search"
 declare   "qp=-file search"
 ###########################################################
